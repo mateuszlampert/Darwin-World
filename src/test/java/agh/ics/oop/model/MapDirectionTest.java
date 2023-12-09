b58757723f61152ae14206dbe3b462d195b3004c
= <szopen1290@gmail.com>
2023-12-09 20:53:48 +0100

    updated tests for new directions

--- src/test/java/agh/ics/oop/model/MapDirectionTest.java
+++ src/test/java/agh/ics/oop/model/MapDirectionTest.java
@@ -8,17 +8,25 @@ public class MapDirectionTest {
 
     @Test
     public void testNext(){
-        assertEquals(MapDirection.EAST, MapDirection.NORTH.next());
-        assertEquals(MapDirection.SOUTH, MapDirection.EAST.next());
-        assertEquals(MapDirection.WEST, MapDirection.SOUTH.next());
-        assertEquals(MapDirection.NORTH, MapDirection.WEST.next());
+        assertEquals(MapDirection.NORTH_EAST, MapDirection.NORTH.next());
+        assertEquals(MapDirection.EAST, MapDirection.NORTH_EAST.next());
+        assertEquals(MapDirection.SOUTH_EAST, MapDirection.EAST.next());
+        assertEquals(MapDirection.SOUTH, MapDirection.SOUTH_EAST.next());
+        assertEquals(MapDirection.SOUTH_WEST, MapDirection.SOUTH.next());
+        assertEquals(MapDirection.WEST, MapDirection.SOUTH_WEST.next());
+        assertEquals(MapDirection.NORTH_WEST, MapDirection.WEST.next());
+        assertEquals(MapDirection.NORTH, MapDirection.NORTH_WEST.next());
     }
 
     @Test
     public void testPrevious(){
-        assertEquals(MapDirection.WEST, MapDirection.NORTH.previous());
-        assertEquals(MapDirection.NORTH, MapDirection.EAST.previous());
-        assertEquals(MapDirection.EAST, MapDirection.SOUTH.previous());
-        assertEquals(MapDirection.SOUTH, MapDirection.WEST.previous());
+        assertEquals(MapDirection.NORTH_WEST, MapDirection.NORTH.previous());
+        assertEquals(MapDirection.NORTH, MapDirection.NORTH_EAST.previous());
+        assertEquals(MapDirection.NORTH_EAST, MapDirection.EAST.previous());
+        assertEquals(MapDirection.EAST, MapDirection.SOUTH_EAST.previous());
+        assertEquals(MapDirection.SOUTH_EAST, MapDirection.SOUTH.previous());
+        assertEquals(MapDirection.SOUTH, MapDirection.SOUTH_WEST.previous());
+        assertEquals(MapDirection.SOUTH_WEST, MapDirection.WEST.previous());
+        assertEquals(MapDirection.WEST, MapDirection.NORTH_WEST.previous());
     }
 }
