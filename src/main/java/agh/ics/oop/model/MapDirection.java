771a6148c0c5f60e007f863d2646593afb14233f
mlampert <mlampert@student.agh.edu.pl>
2023-12-13 12:15:47 +0100

    added constructor to enum in MapDirection

--- src/main/java/agh/ics/oop/model/MapDirection.java
+++ src/main/java/agh/ics/oop/model/MapDirection.java
@@ -1,77 +1,44 @@
 package agh.ics.oop.model;
 
-public enum MapDirection {
-    NORTH,
-    NORTH_WEST,
-    WEST,
-    SOUTH_WEST,
-    SOUTH,
-    SOUTH_EAST,
-    EAST,
-    NORTH_EAST;
+import java.util.Map;
 
-    public static final Vector2d MOVE_VECTOR_NORTH = new Vector2d(0,1);
-    public static final Vector2d MOVE_VECTOR_EAST = new Vector2d(1,0);
-    public static final Vector2d MOVE_VECTOR_SOUTH = new Vector2d(0,-1);
-    public static final Vector2d MOVE_VECTOR_WEST = new Vector2d(-1,0);
-    public static final Vector2d MOVE_VECTOR_NORTH_WEST = new Vector2d(-1, 1);
-    public static final Vector2d MOVE_VECTOR_NORTH_EAST = new Vector2d(1, 1);
-    public static final Vector2d MOVE_VECTOR_SOUTH_WEST = new Vector2d(-1, -1);
-    public static final Vector2d MOVE_VECTOR_SOUTH_EAST = new Vector2d(1, -1);
+public enum MapDirection {
+    NORTH(new Vector2d(0,1), "POLNOC"),
+    NORTH_WEST(new Vector2d(-1, 1), "POLNOCNY ZACHOD"),
+    WEST(new Vector2d(-1,0), "ZACHOD"),
+    SOUTH_WEST(new Vector2d(-1, -1), "POLUDNIOWY ZACHOD"),
+    SOUTH(new Vector2d(0,-1), "POLUDNIE"),
+    SOUTH_EAST(new Vector2d(1, -1), "POLUDNIOWY WSCHOD"),
+    EAST(new Vector2d(1,0), "WSCHOD"),
+    NORTH_EAST(new Vector2d(1, 1), "POLNOCNY WSCHOD");
+
+    private final Vector2d moveVector;
+    private final String directionString;
+
+    private MapDirection(Vector2d moveVector, String directionString){
+        this.moveVector = moveVector;
+        this.directionString = directionString;
+    }
 
     public MapDirection rotate(int rotation){
         return MapDirection.values()[(ordinal() + rotation) % 8];
     }
 
     public String toString() {
-        return switch (this) {
-            case NORTH -> "Polnoc";
-            case SOUTH -> "Poludnie";
-            case WEST -> "Zachod";
-            case EAST -> "Wschod";
-            case NORTH_WEST -> "Polnocny Zachod";
-            case NORTH_EAST -> "Polnocny Wschod";
-            case SOUTH_WEST -> "Poludniowy Zachod";
-            case SOUTH_EAST -> "Poludniowy Wschod";
-        };
+        return this.directionString;
     }
 
+//    TO REMOVE LATER
     public MapDirection next() {
-        return switch (this) {
-            case NORTH -> MapDirection.NORTH_EAST;
-            case NORTH_EAST -> MapDirection.EAST;
-            case EAST -> MapDirection.SOUTH_EAST;
-            case SOUTH_EAST -> MapDirection.SOUTH;
-            case SOUTH -> MapDirection.SOUTH_WEST;
-            case SOUTH_WEST -> MapDirection.WEST;
-            case WEST -> MapDirection.NORTH_WEST;
-            case NORTH_WEST -> MapDirection.NORTH;
-        };
+        return rotate(1);
     }
 
+//    TO REMOVE LATER
     public MapDirection previous() {
-        return switch (this) {
-            case NORTH -> MapDirection.NORTH_WEST;
-            case NORTH_WEST -> MapDirection.WEST;
-            case WEST -> MapDirection.SOUTH_WEST;
-            case SOUTH_WEST -> MapDirection.SOUTH;
-            case SOUTH -> MapDirection.SOUTH_EAST;
-            case SOUTH_EAST -> MapDirection.EAST;
-            case EAST -> MapDirection.NORTH_EAST;
-            case NORTH_EAST -> MapDirection.NORTH;
-        };
+        return rotate(7);
     }
 
     public Vector2d toMoveVector(){
-        return switch (this){
-            case NORTH -> MOVE_VECTOR_NORTH;
-            case NORTH_WEST -> MOVE_VECTOR_NORTH_WEST;
-            case NORTH_EAST -> MOVE_VECTOR_NORTH_EAST;
-            case EAST -> MOVE_VECTOR_EAST;
-            case SOUTH_EAST -> MOVE_VECTOR_SOUTH_EAST;
-            case SOUTH -> MOVE_VECTOR_SOUTH;
-            case SOUTH_WEST -> MOVE_VECTOR_SOUTH_WEST;
-            case WEST -> MOVE_VECTOR_WEST;
-        };
+        return this.moveVector;
     }
 }
