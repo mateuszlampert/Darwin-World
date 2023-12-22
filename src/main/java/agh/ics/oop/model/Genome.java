4bac809822bb565461de2d2fbf756c0d2d67747b
mlampert <mlampert@student.agh.edu.pl>
2023-12-22 23:19:55 +0100

    implemented another constructor for Genome (without specified genes, randomly generating)

--- src/main/java/agh/ics/oop/model/Genome.java
+++ src/main/java/agh/ics/oop/model/Genome.java
@@ -1,11 +1,21 @@
 package agh.ics.oop.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Genome {
     private final List<Integer> genes;
     private int prevMove = -1;
     private final AnimalBehavior animalBehavior;
+
+    public Genome(int genomeLength, AnimalBehavior animalBehavior){
+        this.animalBehavior = animalBehavior;
+        genes = new ArrayList<>(genomeLength);
+
+        for (int i = 0; i < genomeLength; i++){
+            genes.add( (int) (Math.random() * 8));
+        }
+    }
 
     public Genome(List<Integer> genes, AnimalBehavior animalBehavior) {
         this.genes = genes;
