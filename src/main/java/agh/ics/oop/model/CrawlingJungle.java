d57437552d557e7f82de555d5bffc6655549fa6e
mlampert <mlampert@student.agh.edu.pl>
2023-12-29 02:07:32 +0100

    implemented CrawlingJungle class extending AbstractPlantGrowing with all methods

--- src/main/java/agh/ics/oop/model/CrawlingJungle.java
+++ src/main/java/agh/ics/oop/model/CrawlingJungle.java
@@ -1,4 +1,25 @@
 package agh.ics.oop.model;
 
-public class CrawlingJungle implements PlantGrowing{
+import java.util.*;
+
+public class CrawlingJungle extends AbstractPlantGrowing{
+
+    protected CrawlingJungle(int width, int height, int plantsToGrow) {
+        super(width, height, plantsToGrow);
+    }
+
+    @Override
+    public Set<Vector2d> getFavourablePositions(Map<Vector2d, Grass> grasses){
+        Set<Vector2d> favourablePositions = new HashSet<>();
+
+        for (Vector2d position: grasses.keySet()){
+            for (int i = 0; i < 8; i++){
+                Vector2d nextPosition = position.add(currDirection.toMoveVector());
+                currDirection = currDirection.next();
+                favourablePositions.add(nextPosition);
+            }
+        }
+
+        return favourablePositions;
+    }
 }
