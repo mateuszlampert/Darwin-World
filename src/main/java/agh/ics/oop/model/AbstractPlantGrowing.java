a66d1ebb330ddf5a818c550c7295e16a9f4d94b9
mlampert <mlampert@student.agh.edu.pl>
2023-12-29 02:04:02 +0100

    created interface methods for PlantGrowing interface and implemented AbstractPlantGrowing abstract class implementing the interface

---
+++ src/main/java/agh/ics/oop/model/AbstractPlantGrowing.java
@@ -0,0 +1,50 @@
+package agh.ics.oop.model;
+
+import java.util.*;
+
+public abstract class AbstractPlantGrowing implements PlantGrowing {
+    protected final int width;
+    protected final int height;
+    protected final int favourablePlantsToGrow;
+    protected final int unfavourablePlantsToGrow;
+    protected MapDirection currDirection = MapDirection.NORTH;
+
+    protected AbstractPlantGrowing(int width, int height, int plantsToGrow) {
+        this.width = width;
+        this.height = height;
+        this.favourablePlantsToGrow = (int) (Math.random() * plantsToGrow);
+        this.unfavourablePlantsToGrow = plantsToGrow - favourablePlantsToGrow;
+    }
+
+    public Set<Vector2d> generateGrassPositions(Map<Vector2d, Grass> grasses) {
+        Set<Vector2d> newGrassPositions = new HashSet<>();
+        Set<Vector2d> favourablePositions = getFavourablePositions(grasses);
+
+        List<Vector2d> favourablePositionsList = new ArrayList<>(favourablePositions);
+
+        for (int i = 0; i < favourablePlantsToGrow; i++){
+            int toSwap = (int) (Math.random()*(width*height - i) + i);
+            Collections.swap(favourablePositionsList, toSwap, i);
+            newGrassPositions.add(favourablePositionsList.get(i));
+        }
+
+        List<Vector2d> unfavourablePositionsList = new ArrayList<>(width*height - grasses.size() - favourablePositions.size());
+
+        for (int i = 0; i < width; i++){
+            for (int j = 0; j < height; j++){
+                Vector2d currPosition = new Vector2d(i, j);
+                if (!(grasses.containsKey(currPosition) || favourablePositions.contains(currPosition))){
+                    unfavourablePositionsList.add(currPosition);
+                }
+            }
+        }
+
+        for (int i = 0; i < unfavourablePlantsToGrow; i++){
+            int toSwap = (int) (Math.random()*(width*height - i) + i);
+            Collections.swap(unfavourablePositionsList, toSwap, i);
+            newGrassPositions.add(unfavourablePositionsList.get(i));
+        }
+
+        return newGrassPositions;
+    }
+}
--- src/main/java/agh/ics/oop/model/PlantGrowing.java
+++ src/main/java/agh/ics/oop/model/PlantGrowing.java
@@ -1,4 +1,10 @@
 package agh.ics.oop.model;
 
-public interface PlantGrowing {
+import java.util.Map;
+import java.util.Set;
+
+public interface PlantGrowing{
+    Set<Vector2d> generateGrassPositions(Map<Vector2d, Grass> grasses);
+
+    Set<Vector2d> getFavourablePositions(Map<Vector2d, Grass> grasses);
 }
