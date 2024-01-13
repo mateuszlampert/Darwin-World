d451c8d473553f0336fe6da7473cf724b742e29d
mateuszlampert <mateuszlampert@gmail.com>
2024-01-14 00:55:13 +0100

    modified PlantGrowing interface methods and implemented eating plants

--- src/main/java/agh/ics/oop/model/PlantGrowing.java
+++ src/main/java/agh/ics/oop/model/PlantGrowing.java
@@ -4,8 +4,8 @@ import java.util.Map;
 import java.util.Set;
 
 public interface PlantGrowing{
-    Set<Vector2d> determineGrassPositions();
-    Set<Vector2d> getFavourablePositions();
+    Set<Vector2d> determineGrassPositions(MoveValidator validator);
+    Set<Vector2d> getFavourablePositions(MoveValidator validator);
     Set<Vector2d> growGrass(MoveValidator validator);
     void setGrasses(Map<Vector2d, Grass> grasses);
     void setPlantsToGrow(int grasses);
